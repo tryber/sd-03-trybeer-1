691c3a67d2a94fc8c648db8ebe5184fe3311a300
nato-x <alvares.renato@hotmail.com>
2020-09-21 19:43:58 -0300

    eslint fixes

--- back-end/index.js
+++ back-end/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const { errorHandler } = require('./middlewares');
 const routers = require('./routers/index');
-const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
