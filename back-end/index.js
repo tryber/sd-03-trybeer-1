baa88ce9925199e3761fe5744b2136433e13a0f2
Leticia Cristina Duarte Lima <leticiacristinaduartelima@Leticias-MacBook-Air.local>
2020-09-22 17:34:09 -0300

    Correcting errors from lint

--- back-end/index.js
+++ back-end/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const { errorHandler } = require('./middlewares');
 const routers = require('./routers/index');
 
 const app = express();
-
-var cors = require('cors');
 
 app.use(cors());
 
