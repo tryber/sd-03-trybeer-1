6c7c7e1f67f74e1d50976a2c91a1b8bd812330fa
Leticia Cristina Duarte Lima <leticiacristinaduartelima@Leticias-MacBook-Air.local>
2020-09-22 18:15:45 -0300

    Fixing eslint errors

--- front-end/src/context/ContextAplication.js
+++ front-end/src/context/ContextAplication.js
@@ -1,8 +1,9 @@
 import React, { createContext, useState } from 'react';
+import PropTypes from 'prop-types';
 
 export const ContextAplication = createContext();
 
-const AplicationProvider = ({children}) => {
+const AplicationProvider = ({ children }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
@@ -17,11 +18,14 @@ const AplicationProvider = ({children}) => {
   };
 
   return (
-    <ContextAplication.Provider value={context}>
+    <ContextAplication.Provider value={ context }>
       {children}
     </ContextAplication.Provider>
   );
 };
 
+AplicationProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default AplicationProvider;
