0d3c3d5f841beda1b042cf4708f05132d8cdcc97
nato-x <alvares.renato@hotmail.com>
2020-09-24 17:10:44 -0300

    middleware validate token[[

--- back-end/index.js
+++ back-end/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const { errorHandler } = require('./middlewares');
 const routers = require('./routers/index');
+const { errorHandler } = require('./middlewares');
 
 const app = express();
 
