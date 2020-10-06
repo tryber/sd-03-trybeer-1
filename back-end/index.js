0675113d528e874810cfed763d75ce835cd37a7e
nato-re <alvares.renato@hotmail.com>
2020-10-06 14:48:55 -0300

    fix eslint

--- back-end/index.js
+++ back-end/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const path = require('path');
+// const path = require('path');
 
 const routers = require('./routers/index');
 const { errorHandler } = require('./middlewares');
